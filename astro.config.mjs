e4c84e64e90274be229ed7c1232b19e34b6ea10d
Eli Newlin <elinewlin@gmail.com>
2024-06-19 23:47:22 -0500

    fixing links after repository changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: "https://R3lish0.github.io",
-    base: "/squealing-shell"
+    base: "/news-plus-plus"
 });
