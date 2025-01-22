1c8bfae3b6749d4780565076e236aa27f536aec5
Eli Newlin <eli@wsnewlin.com>
2025-01-21 18:18:52 -0600

    almost there

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://news-plus-plus.com/"
+    site: "https://news-plus-plus.com",
+    base: process.env.NODE_ENV === 'production' ? '' : 'news-plus-plus/'
 });
