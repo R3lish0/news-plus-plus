2e1b662c1587f97a8a1f24fb868be5be56e6193b
Eli Newlin <eli@wsnewlin.com>
2025-01-21 18:45:36 -0600

    press edit

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,5 @@ const isProd = process.env.NODE_ENV === 'production';
 // https://astro.build/config
 export default defineConfig({
     site: "https://news-plus-plus.com",
-    base: isProd ? '' : '/news-plus-plus'
+    base: isProd ? '' : '/news-plus-plus/'
 });
