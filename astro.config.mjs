4bed9b78fa8dcbd4f7eebb411204a35f6386095b
Eli Newlin <eli@wsnewlin.com>
2025-01-21 18:35:01 -0600

    I swear

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,5 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
     site: "https://news-plus-plus.com",
     //uncomment this when testing locally
-    //base: import.meta.env.PROD ? '' : 'news-plus-plus'
-});
+    base:'news-plus-plus/'
+})
