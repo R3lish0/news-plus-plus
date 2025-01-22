209863dbd6c9d96e523dd61ebf9a76b515b17e7d
Eli Newlin <eli@wsnewlin.com>
2025-01-21 18:07:28 -0600

    trying again

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: "news-plus-plus.com",
+    site: "https://news-plus-plus.com",
     base: "/news-plus-plus"
 });
