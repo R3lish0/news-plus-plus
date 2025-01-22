1bf2686bab662b9b7cd4b6560f8ecd517ad89680
Eli Newlin <eli@wsnewlin.com>
2025-01-21 18:13:02 -0600

    many link changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://news-plus-plus.com/",
-    base: "/news-plus-plus"
+    site: "https://news-plus-plus.com/"
 });
