96b76da3a5d4d890f35fa3fb465abc0b04e1b857
Eli Newlin <eli@wsnewlin.com>
2025-01-21 18:30:42 -0600

    bruh

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: "https://news-plus-plus.com",
-    base: import.meta.env.PROD ? '' : 'news-plus-plus'
+    //uncomment this when testing locally
+    //base: import.meta.env.PROD ? '' : 'news-plus-plus'
 });
