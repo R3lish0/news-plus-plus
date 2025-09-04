2a4071333e0416164b4734cccbcb828e921d3566
Seth Traman <sethtraman@gmail.com>
2025-09-04 12:21:51 -0500

    Fix build issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,5 @@
 import { defineConfig } from 'astro/config';
 
-const isProd = process.env.NODE_ENV === 'production';
-
 // https://astro.build/config
 export default defineConfig({
     site: "https://news-plus-plus.com",
